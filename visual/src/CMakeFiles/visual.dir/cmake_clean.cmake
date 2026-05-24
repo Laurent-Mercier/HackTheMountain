b6e9643bdaaee2f54ea8f5a67d921088c251bda4
Laurent-Mercier <laurent.mercier@umontreal.ca>
2026-05-24 08:47:20 -0400

    Jad

---
+++ visual/src/CMakeFiles/visual.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/visual.dir/link.d"
+  "CMakeFiles/visual.dir/main.cpp.o"
+  "CMakeFiles/visual.dir/main.cpp.o.d"
+  "visual"
+  "visual.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/visual.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
